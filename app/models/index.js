ffaeb20e5f5cc67bffd5d6b0a8db15680137a358
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-08-30 22:05:49 +0700

    setting db.sequelize on post

--- app/models/index.js
+++ app/models/index.js
@@ -13,3 +13,12 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     idle: dbConfig.pool.idle,
   },
 });
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.posts = require("./post.model.js")(sequelize, Sequelize);
+
+module.exports = db;
