b8b873700f6762f15c5c815396405be130321a7a
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-08-30 20:42:14 +0700

    setting express and cors

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+let whiteList = ["http://localhost:8081"];
+
+let corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
